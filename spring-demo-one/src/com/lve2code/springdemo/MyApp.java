a8c479c3e425d81b907d826cd341bdfc7d9bf65e
DasSonia <soniadas363@gmail.com>
2019-04-03 08:52:21 +0530

    First Test

---
+++ spring-demo-one/src/com/lve2code/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.lve2code.springdemo;
+
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		// create the object
+	     BaseballCoach theCoach = new BaseballCoach();
+		//Coach theCoach = new TrackCoach();
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());		
+	}
+
+}
