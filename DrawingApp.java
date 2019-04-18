a28841698edaa5cc791f40a33d1601efb69e0cc7
DasSonia <soniadas363@gmail.com>
2019-04-18 19:03:50 +0530

    Bean Factory Basic usage

--- DrawingApp.java
+++ DrawingApp.java
@@ -1,10 +1,18 @@
 package com.lve2code.springdemo;
 
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle();
-		triangle.draw();
+	//Triangle triangle = new Triangle();
+		
+	BeanFactory factory = (BeanFactory) (new ClassPathXmlApplicationContext(new String[] {"file:src/com/lve2code/springdemo/spring.xml"}));
+	Triangle triangle = (Triangle)factory.getBean("triangle");
+		
+	triangle.draw();
 	}
 
 }
