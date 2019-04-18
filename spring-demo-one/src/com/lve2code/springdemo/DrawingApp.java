016a2d8acc0d7c442590ae2e564ad8b795432289
DasSonia <soniadas363@gmail.com>
2019-04-18 17:52:25 +0530

    Bean Factory commit 1

---
+++ spring-demo-one/src/com/lve2code/springdemo/DrawingApp.java
@@ -0,0 +1,10 @@
+package com.lve2code.springdemo;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		Triangle triangle = new Triangle();
+		triangle.draw();
+	}
+
+}
---
+++ spring-demo-one/src/com/lve2code/springdemo/Triangle.java
@@ -0,0 +1,7 @@
+package com.lve2code.springdemo;
+
+public class Triangle {
+	public void draw() {
+		System.out.println("Triangle Drawn");
+	}
+}
